eab7392fa052ba9c196096a785bbad710181fe36
Evan-0425 <94693033+Evan-0425@users.noreply.github.com>
2022-08-30 10:50:15 +0200

    finish

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,13 +1,13 @@
 /**
-* Jeu du nombre mystère
-* @author  Steve Fallet
-* @version 0.1
-* @since   2018-09-09 (date de création)
-*/
+ * Jeu du nombre mystère
+ * @author  Evan
+ * @version 0.1
+ * @since   30.08.2022
+ */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
@@ -19,4 +19,20 @@
         return Math.floor(Math.random() * (max - min) + min);
     }
 
+    let nbMystere = tireNombre(1, 100);
+    let nbEssaie = 0;
+    while (true) {
+        let reponse = Number(prompt(` Entrez un nombre en 1 et 100 `));
+        nbEssaie++;
+        if (reponse === nbMystere) {
+            alert(`Vous avez trouvé après ${nbEssaie} essais.`);
+            return;
+        } else {
+            if (reponse > nbMystere) {
+                alert(`C'est moins`);
+            } else {
+                alert(`C'est plus`);
+            }
+        }
+    }
 }()); // main IIFE
